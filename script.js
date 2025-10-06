a6bf995e7ebaa377a46a36ef85220db2c4ec70a5
lateef <117226522+latakin@users.noreply.github.com>
2025-10-06 23:00:38 +0100

    completed the game

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ function GameBoard() {
 
     const getBoard = () => board;
 
-    const dropValue = (row, column, player) => {
-        const cell = board[row][column];
-        if (cell.getValue() === 0) {
+    const dropValue = (row, column , player) => {
+        let cell = board[row][column]
+        if (board.getValue() === '') {
             cell.addValue(player);
         } else {
             console.log("Cell already taken!");
@@ -32,14 +32,55 @@ function GameBoard() {
     return { getBoard, dropValue, printBoard };
 }
 
+
 function Cell() {
-    let value = 0;
+    let value = '';
     const addValue = (player) => {
         value = player;
     };
     const getValue = () => value;
 
     return { getValue, addValue };
+}
+
+function Screen(board, getActivePlayer, switchPlayerTurn, checkWinnerCallback) {
+    const createDiv = () => {
+        const GameD = document.querySelector('.gameboard');
+        GameD.innerHTML = ''; // Clear board on each new round
+        const message = document.querySelector('#playersTurn');
+
+        board.getBoard().forEach((row, rowIndex) => {
+            row.forEach((cell, colIndex) => {
+                let div = document.createElement('div');
+                div.style.backgroundColor = 'black';
+                div.textContent = cell.getValue(); // shows current value
+                div.setAttribute('class', 'cell');
+                div.style.width = '60px';
+                div.style.height = '60px';
+                div.style.border = '1px solid white';
+                div.style.fontSize = '2rem';
+                //div.style.lineHeight = '100px';
+
+                div.addEventListener('click', () => {
+                    if (cell.getValue() === '') {
+                        cell.addValue(getActivePlayer().token);
+                        div.textContent = getActivePlayer().token;
+
+                        if (checkWinnerCallback()) {
+                            message.textContent = `${getActivePlayer().name} wins!`
+                        } else {
+                            switchPlayerTurn();
+                            message.textContent = `${getActivePlayer().name}'s turn`
+                        }
+                    }
+                });
+
+                GameD.appendChild(div);
+            });
+        });
+    };
+
+    return { createDiv };
 }
 
 function GameController() {
@@ -53,25 +94,39 @@ function GameController() {
 
     const switchPlayerTurn = () => {
         activePlayer = activePlayer === players[0] ? players[1] : players[0];
+        updateScreen.createDiv();
     };
 
     const getActivePlayer = () => activePlayer;
 
-    const printNewRound = () => {
-        board.printBoard();
-        console.log(`${getActivePlayer().name}'s turn`);
+    const checkWinner = () => {
+        const b = board.getBoard();
+        const lines = [
+            [b[0][0], b[0][1], b[0][2]],
+            [b[1][0], b[1][1], b[1][2]],
+            [b[2][0], b[2][1], b[2][2]],
+            [b[0][0], b[1][0], b[2][0]],
+            [b[0][1], b[1][1], b[2][1]],
+            [b[0][2], b[1][2], b[2][2]],
+            [b[0][0], b[1][1], b[2][2]],
+            [b[0][2], b[1][1], b[2][0]]
+        ];
+
+        return lines.some(line => {
+            const [a, b, c] = line;
+            return a.getValue() !== '' && a.getValue() === b.getValue() && a.getValue() === c.getValue();
+        });
     };
 
-    const playRound = (row, column) => {
-        console.log(`Dropping ${getActivePlayer().name}'s token into (${row}, ${column})`);
-        board.dropValue(row, column, getActivePlayer().token);
-        switchPlayerTurn();
-        printNewRound();
-    };
+    const updateScreen = Screen(board, getActivePlayer, switchPlayerTurn, checkWinner);
 
-    printNewRound();
+    updateScreen.createDiv(); // Start the game
 
-    return { playRound, getActivePlayer };
+    return {};
 }
 
-const game = GameController();
+GameController();
+
+    
+
+

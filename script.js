a715be2f9637861d88c772b438116b1cf368d2eb
Akinwunmi Lateef <MAC@MacBook-Air.local>
2026-03-19 10:42:29 +0300

    modified styles and some functionality

--- script.js
+++ script.js
@@ -1,137 +1,158 @@
 function GameBoard() {
-    let rows = 3;
-    let columns = 3;
-    let board = [];
+  let rows = 3;
+  let columns = 3;
+  let board = [];
 
-    // Create new Cell for each position
-    for (let i = 0; i < rows; i++) {
-        board[i] = [];
-        for (let j = 0; j < columns; j++) {
-            board[i].push(Cell());
-        }
+  // Create new Cell for each position
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell());
     }
+  }
 
-    const getBoard = () => board;
+  const getBoard = () => board;
 
-    const dropValue = (row, column , player) => {
-        let cell = board[row][column]
-        if (board.getValue() === '') {
-            cell.addValue(player);
-        } else {
-            console.log("Cell already taken!");
-        }
-    };
+  const dropValue = (row, column, player) => {
+    let cell = board[row][column];
+    if (board.getValue() === "") {
+      cell.addValue(player);
+    } else {
+      console.log("Cell already taken!");
+    }
+  };
 
-    const printBoard = () => {
-        const boardWithValues = board.map(row =>
-            row.map(cell => cell.getValue())
-        );
-        console.log(boardWithValues);
-    };
+  const printBoard = () => {
+    const boardWithValues = board.map((row) =>
+      row.map((cell) => cell.getValue()),
+    );
+    console.log(boardWithValues);
+  };
 
-    return { getBoard, dropValue, printBoard };
+  return { getBoard, dropValue, printBoard };
 }
 
-
 function Cell() {
-    let value = '';
-    const addValue = (player) => {
-        value = player;
-    };
-    const getValue = () => value;
+  let value = "";
+  let color = "";
+  const addValue = (player) => {
+    value = player;
+  };
+  const getValue = () => value;
+  const addColor = (player) => {
+    color = player;
+  };
+  const getColor = () => color;
 
-    return { getValue, addValue };
+  return { getValue, addValue, getColor, addColor };
 }
 
 function Screen(board, getActivePlayer, switchPlayerTurn, checkWinnerCallback) {
-     
-    const createDiv = () => {
-        const GameD = document.querySelector('.gameboard');
-        GameD.innerHTML = ''; // Clear board on each new round
-        const message = document.querySelector('#playersTurn');
+  const createDiv = () => {
+    const GameD = document.querySelector(".gameboard");
+    GameD.innerHTML = ""; // Clear board on each new round
+    const message = document.querySelector("#playersTurn");
+    //const players = ["PlayerOne", "PlayerTwo"];
+    let divClick = true;
+    board.getBoard().forEach((row, rowindex) => {
+      row.forEach((cell, cellindex) => {
+        let div = document.createElement("div");
+        let playerText = document.createElement("h2");
+        div.appendChild(playerText);
+        div.style.backgroundColor = "black";
+        playerText.textContent = cell.getValue();
+        playerText.style.color = cell.getColor();
 
-        board.getBoard().forEach((row, rowIndex) => {
-            row.forEach((cell, colIndex) => {
-                let div = document.createElement('div');
-                div.style.backgroundColor = 'black';
-                div.textContent = cell.getValue(); // shows current value
-                div.setAttribute('class', 'cell');
-                div.style.width = '60px';
-                div.style.height = '60px';
-                div.style.border = '1px solid white';
-                div.style.fontSize = '2rem';
-                //div.style.lineHeight = '100px';
+        div.style.width = "120px";
+        div.style.height = "120px";
+        div.style.border = "1px solid white";
+        div.style.fontSize = "3rem";
 
-                div.addEventListener('click', () => {
-                    if (cell.getValue() === '') {
-                        cell.addValue(getActivePlayer().token);
-                        div.textContent = getActivePlayer().token;
+        //div.style.lineHeight = '100px';
 
-                        if (checkWinnerCallback()) {
-                            message.textContent = `${getActivePlayer().name} wins!`
-                        } else {
-                            switchPlayerTurn();
-                            message.textContent = `${getActivePlayer().name}'s turn`
-                        }
-                    }
-                });
+        div.addEventListener("click", () => {
+          if (divClick) {
+            if (cell.getValue() === "" && cell.getColor() === "") {
+              cell.addValue(getActivePlayer().token);
+              cell.addColor(getActivePlayer().color);
+              playerText.style.color = getActivePlayer().color;
+              playerText.textContent = getActivePlayer().token;
 
-                GameD.appendChild(div);
-            });
+              if (checkWinnerCallback() && divClick) {
+                message.textContent = `${getActivePlayer().name} wins! this round✅`;
+                divClick = false;
+                console.log(divClick);
+              } else {
+                switchPlayerTurn();
+                message.textContent = `${getActivePlayer().name}'s turn`;
+              }
+            }
+          }
         });
-    };
 
-    return { createDiv };
+        GameD.appendChild(div);
+      });
+    });
+  };
+
+  return { createDiv };
 }
 
 function GameController() {
-    const board = GameBoard();
-   
-    const players = [
-        { name: 'PlayerOne', token: 'X' },
-        { name: 'PlayerTwo', token: 'O' }
+  const board = GameBoard();
+
+  const players = [
+    { name: "PlayerOne", token: "X", color: "Red" },
+    { name: "PlayerTwo", token: "O", color: "Blue" },
+  ];
+
+  let activePlayer = players[0];
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    updateScreen.createDiv();
+  };
+
+  const getActivePlayer = () => activePlayer;
+
+  const checkWinner = () => {
+    const b = board.getBoard();
+    const lines = [
+      [b[0][0], b[0][1], b[0][2]],
+      [b[1][0], b[1][1], b[1][2]],
+      [b[2][0], b[2][1], b[2][2]],
+      [b[0][0], b[1][0], b[2][0]],
+      [b[0][1], b[1][1], b[2][1]],
+      [b[0][2], b[1][2], b[2][2]],
+      [b[0][0], b[1][1], b[2][2]],
+      [b[0][2], b[1][1], b[2][0]],
     ];
 
-    let activePlayer = players[0];
+    return lines.some((line) => {
+      const [a, b, c] = line;
+      return (
+        a.getValue() !== "" &&
+        a.getValue() === b.getValue() &&
+        a.getValue() === c.getValue()
+      );
+    });
+  };
 
-    const switchPlayerTurn = () => {
-        activePlayer = activePlayer === players[0] ? players[1] : players[0];
-        updateScreen.createDiv();
-    };
+  const updateScreen = Screen(
+    board,
+    getActivePlayer,
+    switchPlayerTurn,
+    checkWinner,
+  );
 
-    const getActivePlayer = () => activePlayer;
+  updateScreen.createDiv(); // Start the game
 
-    const checkWinner = () => {
-        const b = board.getBoard();
-        const lines = [
-            [b[0][0], b[0][1], b[0][2]],
-            [b[1][0], b[1][1], b[1][2]],
-            [b[2][0], b[2][1], b[2][2]],
-            [b[0][0], b[1][0], b[2][0]],
-            [b[0][1], b[1][1], b[2][1]],
-            [b[0][2], b[1][2], b[2][2]],
-            [b[0][0], b[1][1], b[2][2]],
-            [b[0][2], b[1][1], b[2][0]]
-        ];
-
-        return lines.some(line => {
-            const [a, b, c] = line;
-            return a.getValue() !== '' && a.getValue() === b.getValue() && a.getValue() === c.getValue();
-        });
-    };
-
-    const updateScreen = Screen(board, getActivePlayer, switchPlayerTurn, checkWinner);
-
-    updateScreen.createDiv(); // Start the game
-
-    return {};
+  return {};
 }
 
 GameController();
 
-const restart = document.querySelector('#restart');
-    restart.addEventListener('click', () => {
-        window.location.reload();
-    })    
-
-
+const restart = document.querySelector("#restart");
+restart.addEventListener("click", () => {
+  window.location.reload();
+});
